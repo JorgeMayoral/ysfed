f58864fd5738871213874b9bff6b4080cf9e1e3f
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2024-01-08 10:48:44 +0100

    feat(lib): :sparkles: add encrypt/decrypt functions

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -17,3 +17,38 @@ pub fn decrypt_file(password: &[u8], input_path: &Path, output_path: &Path) -> R
         Err(e) => Err(format!("Could not decrypt file: {e:?}")),
     }
 }
+
+pub fn decrypt_input(password: &[u8], input: &[u8]) -> Result<String, String> {
+    let cocoon = Cocoon::new(password);
+    let output = cocoon.unwrap(input);
+    if output.is_err() {
+        let e = output.err().unwrap();
+        return Err(format!("Could not decrypt input: {e:?}"));
+    }
+    let output = output.unwrap();
+    let output = String::from_utf8(output);
+    match output {
+        Ok(output) => Ok(output),
+        Err(e) => Err(format!("Could not decrypt input: {e:?}")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::encrypt::encrypt_input;
+
+    use super::*;
+
+    #[test]
+    fn test_decrypt_input_ok() {
+        let password = "password".as_bytes();
+        let input = "This is a test".to_string();
+        let encrypted = encrypt_input(password, input.clone());
+        assert!(encrypted.is_ok());
+        let encrypted = encrypted.unwrap();
+        let decrypted = decrypt_input(password, &encrypted);
+        assert!(decrypted.is_ok());
+        let decrypted = decrypted.unwrap();
+        assert_eq!(decrypted, input);
+    }
+}
--- src/encrypt.rs
+++ src/encrypt.rs
@@ -11,3 +11,28 @@ pub fn encrypt_file(password: &[u8], input_path: &Path, output_path: &Path) -> R
         .dump(data, &mut file)
         .map_err(|e| format!("Could not encrypt file: {e:?}"))
 }
+
+pub fn encrypt_input(password: &[u8], input: String) -> Result<Vec<u8>, String> {
+    let mut cocoon = Cocoon::new(password);
+    let data = input.as_bytes();
+    let output = cocoon.wrap(data);
+    match output {
+        Ok(output) => Ok(output),
+        Err(e) => Err(format!("Could not encrypt input: {e:?}")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encrypt_input_ok() {
+        let password = "password".as_bytes();
+        let input = "This is a test".to_string();
+        let output = encrypt_input(password, input.clone());
+        assert!(output.is_ok());
+        let output = output.unwrap();
+        assert_ne!(output, input.as_bytes());
+    }
+}
